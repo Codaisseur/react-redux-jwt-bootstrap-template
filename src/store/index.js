9d6fe1b01a06f765056b29763c16d70875a21f9b
Reinoptland <reinoptland@gmail.com>
2020-03-27 19:30:23 +0100

    Prevent app from crashing when redux devtools is not installed

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
+const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
 
 const store = createStore(reducer, enhancer);
 
