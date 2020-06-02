e2e76b6cb018843561c361d3e8593b88fcca8cad
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-02 17:36:05 +0200

    update mypage

--- src/store/homepages/actions.js
+++ src/store/homepages/actions.js
@@ -21,3 +21,31 @@ export const fetchHomepages = () => async (dispatch, getState) => {
     console.log(error);
   }
 };
+
+export const updateHomepage = (id, newHomepage) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    const { token } = getState().user;
+    const { title, description, color, bgColor } = newHomepage;
+    const response = await axios.patch(
+      `http://localhost:4000/homepages/${id}`,
+      {
+        title,
+        description,
+        color,
+        backgroundColor: bgColor,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    dispatch(saveUserHomepage(response.data));
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+  }
+};
